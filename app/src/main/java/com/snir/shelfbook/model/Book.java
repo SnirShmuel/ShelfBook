f4af17754fc4231ee61acca41f1b1bc42423ec18
SnirShmuel <37809417+SnirShmuel@users.noreply.github.com>
2021-01-20 03:27:43 +0200

    add dependencies and book class

---
+++ app/src/main/java/com/snir/shelfbook/model/Book.java
@@ -0,0 +1,66 @@
+package com.snir.shelfbook.model;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
+public class Book {
+    @PrimaryKey
+    @NonNull
+    private String id;
+    private String name;
+    private String bookCondition;
+    private String genre;
+    private String imageUrl;
+    private String description;
+
+    @NonNull
+    public String getId() {
+        return id;
+    }
+
+    public void setId(@NonNull String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBookCondition() {
+        return bookCondition;
+    }
+
+    public void setBookCondition(String bookCondition) {
+        this.bookCondition = bookCondition;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
