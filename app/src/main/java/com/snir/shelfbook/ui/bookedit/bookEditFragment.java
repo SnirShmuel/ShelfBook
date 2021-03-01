fdf9abe2d0d0e0ab943a6977db50852d3b5a238c
SnirShmuel <37809417+SnirShmuel@users.noreply.github.com>
2021-03-01 16:48:38 +0200

    fix edit book + pass values from detail to edit

--- app/src/main/java/com/snir/shelfbook/ui/bookedit/bookEditFragment.java
+++ app/src/main/java/com/snir/shelfbook/ui/bookedit/bookEditFragment.java
@@ -7,10 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.snir.shelfbook.R;
+import com.snir.shelfbook.model.book.Book;
 
 public class bookEditFragment extends Fragment {
+    Book book;
+    EditText bookNameEv;
+    EditText bookGenreEv;
+    EditText bookConditionEv;
+    EditText bookDescriptionEv;
+    Button saveBtn;
+    Button deleteBtn;
 
     public bookEditFragment() {
         // Required empty public constructor
@@ -21,6 +31,20 @@ public class bookEditFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_book_edit, container, false);
+
+        book= bookEditFragmentArgs.fromBundle(getArguments()).getBook();
+
+        bookNameEv = view.findViewById(R.id.bookEdit_nameEv);
+        bookGenreEv = view.findViewById(R.id.bookEdit_GenreEv);
+        bookConditionEv = view.findViewById(R.id.bookEdit_conditionEv);
+        bookDescriptionEv = view.findViewById(R.id.bookEdit_descEv);
+        saveBtn = view.findViewById(R.id.bookEdit_saveEditBtn);
+        deleteBtn = view.findViewById(R.id.bookEdit_deleteBtn);
+
+        bookNameEv.setText(book.getName());
+        bookGenreEv.setText(book.getGenre());
+        bookConditionEv.setText(book.getBookCondition());
+        bookDescriptionEv.setText(book.getDescription());
 
         return view;
     }
